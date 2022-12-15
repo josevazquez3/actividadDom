1870a9fad31ecf0dd41003bdba585374d3467765
jose vazquez <josevazquez3@gmail.com>
2022-12-15 20:53:57 -0300

    cree una funcion para añadir personas al DOM

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ const persona2 ={
     image: "https://i.imgur.com/ifEu2gV.jpeg"
 }
 
+function appendPersona (persona) {
+    const listado = document.getElementById ("listado") 
+    const div = document.createElement ("div")
+    const imagen = document.createElement ("img")
+    const titulo = document.createElement ("h4")
+    const parrafo = document.createElement ("p")
+    const parrafo2 = document.createElement ("p")
+    imagen.src = persona.image 
+    titulo.textContent=persona.name+persona.lastname
+    parrafo.textContent=persona.profession
+    parrafo2.textContent=persona.age+"años"
+    div.append (imagen,titulo, parrafo, parrafo2)
+    listado.append (div)
+    }
+appendPersona (persona1)
+appendPersona (persona2)
+
