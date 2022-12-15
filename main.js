fd78e90940a1a09fee0cd5d5cd59140a1829ae9e
jose vazquez <josevazquez3@gmail.com>
2022-12-15 20:13:24 -0300

    cree las personas

---
+++ main.js
@@ -0,0 +1,15 @@
+const persona1 ={
+    name: "Jose",
+    lastname: "vazquez",
+    age: 46,
+    profession: "contable",  
+    image: "https://i.imgur.com/ifEu2gV.jpeg"
+    }
+const persona2 ={
+    name: "Juan",
+    lastname: "Perez",
+    age: 25,
+    profession: "Ingeniero",  
+    image: "https://i.imgur.com/ifEu2gV.jpeg"
+}
+
